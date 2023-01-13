d196b505328212a30ebfcbb1609507ee76695185
Rezab Ud Dawla <rezabuddawlatuhin@gmail.com>
2023-01-13 13:09:53 +0600

    basic structure

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mychatapp/screens/auntheticate/login.dart';
+import 'package:mychatapp/screens/auntheticate/registraion.dart';
+import 'package:mychatapp/screens/wrapper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: Wrapper());
+  }
+}
